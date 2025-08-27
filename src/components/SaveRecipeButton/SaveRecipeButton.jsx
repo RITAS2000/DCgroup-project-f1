5b25b4e5de53d24b1afc827e0b8367a32e70e392
ROksana-Zhuk <roksi.sun.forever@gmail.com>
2025-08-28 00:39:12 +0300

    fix icon and save button

--- src/components/SaveRecipeButton/SaveRecipeButton.jsx
+++ src/components/SaveRecipeButton/SaveRecipeButton.jsx
@@ -13,92 +13,114 @@ import 'react-toastify/dist/ReactToastify.css';
 import { openModal } from '../../redux/modal/slice.js';
 
 export default function SaveRecipeButton({ recipeId }) {
-  const [isSaved, setIsSaved] = useState(false);
 
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const dispatch = useDispatch();
+    const [isSaved, setIsSaved] = useState(false);
 
-  useEffect(() => {
-    const checkIfRecipeIsSaved = async () => {
-      try {
-        if (isLoggedIn) {
-          const result = await getSavedRecipes();
-          const savedRecipes = result.data.data || [];
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+    const dispatch = useDispatch();
 
-          const found = savedRecipes.find((recipe) => recipe.id === recipeId);
-          setIsSaved(found);
-        }
-      } catch (error) {
-        console.error('Error fetching saved recipes:', error);
-      }
+
+    useEffect(() => {
+        const checkIfRecipeIsSaved = async () => {
+            try {
+                if (isLoggedIn) {
+                    const result = await getSavedRecipes();
+
+
+                    const savedRecipes = result.data.data || [];
+
+
+                    const found = savedRecipes.find(recipe => recipe._id === recipeId);
+                    setIsSaved(found);
+                }
+            } catch (error) {
+                console.error("Error fetching saved recipes:", error);
+            }
+        };
+
+        checkIfRecipeIsSaved();
+    }, [isLoggedIn, recipeId]);
+
+
+    const handleUnsave = () => {
+        const deleteSavedRecipe = async () => {
+            try {
+
+
+                if (isLoggedIn) {
+                    await delSavedRecipes(recipeId);
+                    setIsSaved(false);
+                } else {
+                    console.log('not logged in');
+                    dispatch(openModal({}));
+                };
+
+            } catch (error) {
+                toast.error(`Error deleting saved recipe: ${error}`);
+            }
+        };
+        deleteSavedRecipe();
     };
 
-    checkIfRecipeIsSaved();
-  }, [isLoggedIn, recipeId]);
 
-  console.log('log', isLoggedIn);
-  console.log('saved', isSaved);
+    const handleSave = () => {
+        const addSavedRecipe = async () => {
+            try {
+                if (isLoggedIn) {
+                    await postSavedRecipes(recipeId);
 
-  const handleUnsave = () => {
-    const deleteSavedRecipe = async () => {
-      try {
-        console.log('recipeId', recipeId);
+                    setIsSaved(true);
+                } else {
+                    console.log('not logged in');
+                    dispatch(openModal({ type: 'notAuthorized' }));
+                };
 
-        if (isLoggedIn) {
-          await delSavedRecipes(recipeId);
-          setIsSaved(false);
-        } else {
-          console.log('not logged in');
-          dispatch(openModal({}));
-        }
-      } catch (error) {
-        toast.error(`Error deleting saved recipe: ${error}`);
-      }
+            } catch (error) {
+            toast.error(`Error adding saved recipe: ${error}` );
+            }
+        };
+        addSavedRecipe();
     };
-    deleteSavedRecipe();
-  };
 
-  const handleSave = () => {
-    const addSavedRecipe = async () => {
-      try {
-        if (isLoggedIn) {
-          await postSavedRecipes(recipeId);
+//     return (
+//         <>
+//           {isSaved ? (
+//             <button onClick={handleUnsave} className={css.saveButton}>
+//               Unsave
+//               <svg className={css.icon} width="24" height="24">
+//                 <use href="/sprite/symbol-defs.svg#icon-bookmark-outline"
+//                      style={{ fill: 'white', stroke: 'none' }}/>
+//               </svg>
+//             </button>
+//           ) : (
+//             <button onClick={handleSave} className={css.saveButton}>
+//               Save
+//               <svg className={css.icon} width="24" height="24">
+//                 <use href="/sprite/symbol-defs.svg#icon-bookmark-outline"
+//                      style={{ fill: 'transparent', stroke: 'white' }}/>
+//               </svg>
+//             </button>
+//           )}
+//         </>
+//     )
+// };
 
-          setIsSaved(true);
-        } else {
-          console.log('not logged in');
-          dispatch(openModal({}));
-        }
-      } catch (error) {
-        toast.error(`Error adding saved recipe: ${error}`);
-      }
-    };
-    addSavedRecipe();
-  };
-
-  return (
+return (
     <>
-      {isSaved ? (
-        <button onClick={handleUnsave} className={css.saveButton}>
-          Unsave
-          <svg className={css.icon} width="24" height="24">
-            <use
-              href="/sprite/symbol-defs.svg#icon-bookmark-outline"
-              style={{ fill: 'white', stroke: 'none' }}
-            />
-          </svg>
-        </button>
-      ) : (
-        <button onClick={handleSave} className={css.saveButton}>
-          Save
-          <svg className={css.icon} width="24" height="24">
-            <use
-              href="/sprite/symbol-defs.svg#icon-bookmark-outline"
-              style={{ fill: 'transparent', stroke: 'white' }}
-            />
-          </svg>
-        </button>
-      )}
+      <button
+        onClick={isSaved ? handleUnsave : handleSave}
+        className={css.saveButton}
+      >
+        {isSaved ? 'Unsave' : 'Save'}
+        <svg
+          className={css.icon}
+          width="24"
+          height="24"
+          style={{ color: 'white' }}
+        >
+          <use href={`/sprite/symbol-defs.svg#${isSaved ? 'icon-bookmark-filled' : 'icon-bookmark-outline'}`} />
+        </svg>
+      </button>
     </>
   );
 }
