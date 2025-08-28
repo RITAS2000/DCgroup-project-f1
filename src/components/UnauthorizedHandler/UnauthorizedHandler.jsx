9ee0498b5f4a6796c426a943a0ec0b7ab767964d
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-29 02:59:37 +0300

    fix

--- src/components/UnauthorizedHandler/UnauthorizedHandler.jsx
+++ src/components/UnauthorizedHandler/UnauthorizedHandler.jsx
@@ -35,13 +35,13 @@ const UnauthorizedHandler = () => {
       if (!tokenMissing && !isAllowed) dispatch(logout());
       dispatch(clearAuth());
       localStorage.removeItem('token');
-      toast.error('Session has expired. Please log in again. тест 1');
+      toast.error('Session has expired. Please log in again.');
       navigate('/');
     }
     if (usersError?.status === 404) {
       dispatch(clearAuth());
       localStorage.removeItem('token');
-      toast.error('Session has expired. Please log in again. тест 2');
+      toast.error('Session has expired. Please log in again.');
       navigate('/');
     }
   }, [
