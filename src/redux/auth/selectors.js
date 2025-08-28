d5b64db3d3a02b9276b7b93d69bb568417d75841
DmytroValMan <dmitri.m2011@gmail.com>
2025-08-28 16:20:10 +0300

    add token

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,3 +2,5 @@ export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 
 export const selectUser = (state) => state.auth.user;
 export const selectIsRegistered = (state) => state.auth.isRegistered;
+
+export const selectToken = (state) => state.auth.token;
